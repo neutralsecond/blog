c11bd00648864ea5b615e48f2ac481901307e5f9
abu <ucanihl@gmail.com>
2021-03-19 16:12:49 +0300

    you can now filter posts by author via clicking on author name

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('author/<pk>/', views.blogger_post_list, name='blogger_post_list')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
@@ -12,6 +13,11 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def blogger_post_list(request, pk):
+    author = User.objects.get(pk=pk)
+    posts = Post.objects.filter(author=author).order_by('date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 @login_required
 def post_new(request):
